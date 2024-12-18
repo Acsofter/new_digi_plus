36134487b7e289a4b2c3dc07bbcadef54371f9d0
Acsofter <32777537+Acsofter@users.noreply.github.com>
2024-12-18 08:49:00 -0400

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -15,12 +15,11 @@ from pathlib import Path
 # from dotenv import load_dotenv
 import os 
 
-# load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 from pathlib import Path
 from decouple import config
@@ -178,37 +177,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-
-# HTTPS
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
-
-
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-
-AWS_LOCATION = config('AWS_LOCATION')
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-TEMP = os.path.join(BASE_DIR, 'temp')
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
